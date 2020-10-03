a76c2b30dbb6a95e052dc65ec975890caf7d8845
John <john@email.com>
2020-10-03 10:32:21 +0900

    Heroku config files and updates

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#&pkgioip#gg5=hmpetqlo7v8k5-tk@p93(!$tn^i%flkx0l_9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
